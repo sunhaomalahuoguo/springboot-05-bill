145987f59254046c8a834456c7fd1131e9cf51da
sunhaomalahuoguo <sunhao20032003@163.com>
2019-04-10 18:17:24 +0800

    第一次提交

---
+++ src/main/java/com/mengxugu/springboot/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.mengxugu.springboot.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+import sun.awt.SunHints;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @PostMapping(value = "/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Map<String,Object> map,
+                        HttpSession session){
+        if (!StringUtils.isEmpty(username) && "111".equals(password)){
+            //登录成功，跳转到主页面
+            session.setAttribute("loginUser",username);
+            return "redirect:/main.html";
+        }
+        //登录失败，跳转到登陆页面
+        map.put("msg","登录失败，用户名或密码错误");
+        return "main/login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute("loginUser");
+        session.invalidate();
+        logger.info("用户登出");
+        return "main/login";
+    }
+}
--- src/main/java/com/mengxugu/springboot/interceptor/LoginHandlerInterceptor.java
+++ src/main/java/com/mengxugu/springboot/interceptor/LoginHandlerInterceptor.java
@@ -1,7 +1,26 @@
 package com.mengxugu.springboot.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class LoginHandlerInterceptor implements HandlerInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+    Logger logger = LoggerFactory.getLogger(LoginHandlerInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if (loginUser != null){
+            //从session中能取出登录用户，说明登录过，直接放行
+            logger.info("拦截器校验-用户已经登录");
+            return true;
+        }
+        //为登录,转发到登陆页面
+        request.setAttribute("msg","没有权限，请先登录！");
+        logger.info("拦截器校验-用户未登录，没有权限,请先登录");
+        request.getRequestDispatcher("/index.html").forward(request,response);
+        return false;
+    }
 }
