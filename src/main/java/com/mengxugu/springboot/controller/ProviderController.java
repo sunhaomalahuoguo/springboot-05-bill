a9d277a8cd4678a51b0f7682f40c2c9c69db74bd
sunhaomalahuoguo <sunhao20032003@163.com>
2019-04-10 18:44:13 +0800

    第一次提交

--- src/main/java/com/mengxugu/springboot/controller/ProviderController.java
+++ src/main/java/com/mengxugu/springboot/controller/ProviderController.java
@@ -1,13 +1,24 @@
 package com.mengxugu.springboot.controller;
 
+import com.mengxugu.springboot.dao.ProviderDao;
+import com.mengxugu.springboot.entities.Provider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Map;
 
 @Controller
 public class ProviderController {
 
+    @Autowired
+    private ProviderDao providerDao;
     @GetMapping("/providers")
-    public String list(){
+    public String list(Map<String,Object> map){
+        Collection<Provider> providers = providerDao.getAll();
+        map.put("providers",providers);
         return "provider/list";
     }
+
 }
